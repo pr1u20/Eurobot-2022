f81c72d3ac39b24d232ad245c083ef792438688f
Pedro Rodriguez <pr1u20@soton.ac.uk>
2024-02-05 11:47:47 +0000

    Change in Aruco size

--- Vision_System/calibration/generate_ChAruco.py
+++ Vision_System/calibration/generate_ChAruco.py
@@ -7,18 +7,18 @@ import Vision_System.parameters as param
 # meant for calibration
 # the following call gets a ChArUco board of tiles 5 wide X 7 tall
 SQUARES_VERTICALLY = param.CHARUCO_SQUARES_VERTICALLY    # 180 < 210 mm
-SQUARES_HORIZONTALLY = param.CHARUCO_SQUARES_VERTICALLY  # 240 < 297 mm
+SQUARES_HORIZONTALLY = param.CHARUCO_SQUARES_HORIZONTALLY  # 240 < 297 mm
 
     
 gridboard = aruco.CharucoBoard(
-        (param.CHARUCO_SQUARES_VERTICALLY, param.CHARUCO_SQUARES_VERTICALLY),  #SquareX, SquareY
+        (param.CHARUCO_SQUARES_VERTICALLY, param.CHARUCO_SQUARES_HORIZONTALLY),  #SquareX, SquareY
         squareLength= param.CHARUCO_squareLength, 
         markerLength=param.CHARUCO_markerLength, 
         dictionary=param.CHARUCO_DICT)
 
 # Create an image from the gridboard
-size_ratio = SQUARES_HORIZONTALLY / SQUARES_VERTICALLY
-board_image = gridboard.generateImage((1000, int(1000*size_ratio)), None, 0, 1)
+size_ratio = param.CHARUCO_SQUARES_HORIZONTALLY / param.CHARUCO_SQUARES_VERTICALLY
+board_image = gridboard.generateImage((1500, int(1500*size_ratio)), None, 0, 1)
 
 path = os.path.join("Vision_System", "calibration", "test_charuco.jpg")
 cv2.imwrite(path, board_image)
--- Vision_System/parameters.py
+++ Vision_System/parameters.py
@@ -7,11 +7,11 @@ aruco_ID42_side_lenght = 0.0455
 
 # CHARUCO CALIBRATION CONSTANTS
 # the following call gets a ChArUco board of tiles 6 wide X 8 tall
-CHARUCO_SQUARES_VERTICALLY = 6    # 180 < 210 mm
-CHARUCO_SQUARES_HORIZONTALLY = 8  # 240 < 297 mm
+CHARUCO_SQUARES_VERTICALLY = 5    # 180 < 210 mm or 250 < 297 mm
+CHARUCO_SQUARES_HORIZONTALLY = 7  # 240 < 297 mm or 385 < 420 mm
 
-CHARUCO_squareLength=0.030
-CHARUCO_markerLength=0.015
+CHARUCO_squareLength=0.055
+CHARUCO_markerLength=0.03
 
 CHARUCO_DICT = aruco.getPredefinedDictionary(aruco.DICT_5X5_50)
 
